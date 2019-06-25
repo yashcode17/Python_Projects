32cd40159a52fa0b24db62aeed9dec5814f54866
Yashasvi Gupta <52156734+yashcode17@users.noreply.github.com>
2019-06-25 15:05:14 +0530

    Add files via upload

---
+++ fullEmailSending.py
@@ -0,0 +1,32 @@
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+msg=MIMEMultipart()    # create object of MIMEMultipart
+
+msg['From'] = 'yashg1709@gail.com'
+msg['To'] = 'shuchiiii1234@gmail.com'
+msg['Subject'] = "Subject of Mail"
+filename = r"C:\Users\HP\Desktop\scc_notes.txt"
+
+with open(filename, 'r') as f:
+    message = MIMEText(f.read(),
+                       _subtype="txt")
+    message.add_header('Content-Disposition',
+                       'attachment',
+                       filename=filename) # attach file
+    msg.attach(message)
+
+body = "Your MEssage Here"
+msg.attach(MIMEText(body, 'plain'))
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+
+server.login('yashg1709@gmail.com','yash888888')
+
+text = msg.as_string()
+server.sendmail(msg['From'],msg['To'], text)
+server.quit()
+
+print("Email send Successfully")
